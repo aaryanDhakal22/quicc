0c482a0f1be040d2313a1644d8f1b4dd214eb50d
Aaryan <47253380+aaryanDhakal22@users.noreply.github.com>
2024-06-14 07:21:47 -0400

    removed useless views
    Using react for frontend instead of django-templates

--- backend/employee/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import employee_home_page,toggle_clock
-
-urlpatterns = [
-    path("", employee_home_page, name="employee_home"),
-    path("toggle_clock/<str:username>/",toggle_clock, name="toggle_clock")
-]
--- backend/manager/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import manager_home_page
-
-urlpatterns = [
-    path("", manager_home_page, name="manager_home"),
-]
